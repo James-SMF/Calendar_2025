c495b7d4d1fbd85c4afb9d75971771816db6e665
李光耀 <gli38@ucsc.edu>
2025-09-27 23:04:51 +0800

    加上app图标

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from utils.data_manager import DataManager
 from scripts.ui_manifester import UIManifester
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon
+from PyQt5.QtGui import QIcon
 import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     manager = DataManager('data/data.txt')
+
+    tray_icon = QSystemTrayIcon(QIcon("showgo.PNG"), app)
+    tray_icon.show()
+    app.setWindowIcon(QIcon("icon.icns"))
+
     window = UIManifester(manager, 10)
     window.show()
     sys.exit(app.exec_())
